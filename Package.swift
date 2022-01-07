47d0997ac7661f180e8629f63f25595fc0ffe369
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-01-07 12:40:41 +0100

    0.0.2

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "OpenMensaMealAdapters",
-    platforms: [.iOS(.v13),.macOS(.v12)],
+    platforms: [.iOS(.v15),.macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "MealPorts", url: "https://github.com/Italbytz/spm-ports-meal.git", from: "0.0.7"),
-        .package(name: "OpenMensaInfrastructure", url: "https://github.com/Italbytz/spm-infrastructure-openmensa.git", from: "0.0.4"),
+        .package(name: "OpenMensaInfrastructure", url: "https://github.com/Italbytz/spm-infrastructure-openmensa.git", from: "0.0.5"),
 
     ],
     targets: [
